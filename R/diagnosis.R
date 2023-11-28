fc8b6a7a51119fc590a505977909a8852bc3bbc7
Tingxuan Wu <txwu0310@gmail.com>
2023-11-27 23:25:22 -0600

    update

--- R/diagnosis.R
+++ R/diagnosis.R
@@ -383,7 +383,7 @@ boxplot.zresid <- function(Zresidual,index=1,
 
 
 #' @export plot.zresid
-plot.zresid <- function(Zresidual,index=1,
+plot.zresid <- function(Zresidual,index=1,ylab = "Z-Residual",
                         X = c("index", "lp", "covariate"),
                         main.title = "Z-residual Scatterplot",
                         outlier.return = FALSE,
@@ -408,7 +408,7 @@ plot.zresid <- function(Zresidual,index=1,
   if (X == "index") {
     plot.default (
       Zresidual[,j],
-      ylab = "Z-Residual",
+      ylab = ylab,
       ylim = c(-ylim0, ylim0 + 1),
       col = c("blue", "darkolivegreen4")[censored + 1],
       #col = ifelse(is.outlier, "darkgoldenrod2", ifelse(censored,"darkolivegreen4","blue")),
@@ -456,7 +456,7 @@ plot.zresid <- function(Zresidual,index=1,
     plot(
       fitted.value,
       Zresidual[,j],
-      ylab = "Z-Residual",
+      ylab = ylab,
       ylim = c(-ylim0, ylim0 + 1),
       col = c("blue", "darkolivegreen4")[censored + 1],
       #col = ifelse(is.outlier, "darkgoldenrod2", ifelse(censored,"darkolivegreen4","blue")),
@@ -520,7 +520,7 @@ plot.zresid <- function(Zresidual,index=1,
     plot(
       fitted.value[,i],
       Zresidual[,j],
-      ylab = "Z-Residual",
+      ylab = ylab,
       ylim = c(-ylim0, ylim0 + 1),
       col = c("blue", "darkolivegreen4")[censored + 1],
       #col = ifelse(is.outlier, "darkgoldenrod2", ifelse(censored,"darkolivegreen4","blue")),
@@ -584,3 +584,67 @@ plot.zresid <- function(Zresidual,index=1,
   }
 
 }
+
+#' @export plot.cs.residual
+plot.cs.residual <- function(cs.residual,ylab = "Cox-Snell Residual",
+                             main.title = "Cox-Snell Residuals Scatterplot",
+                             outlier.return = FALSE,
+                             ...)
+{
+  censored <- attr(cs.residual, "censored.status")
+  km <- survfit(Surv(cs.residual, censored)~1,type='fleming')
+  id <- order(cs.residual)
+
+  id.infinity <- which (!is.finite(cs.residual))
+  if (length(id.infinity) > 0L) {
+    value.notfinite <- as.character.na(cs.residual[id.infinity])
+    max.non.infinity <- max(abs(cs.residual[-id.infinity]))
+    cs.residual[id.infinity] <-
+      sign.na(cs.residual[id.infinity]) * (max.non.infinity + 0.1)
+    message("Non-finite Zresiduals exist! The model or the fitting process has a problem!")
+  }
+
+  is.outlier <- (abs(cs.residual) > 3.5)
+
+  plot(km, fun="cumhaz", xlab=("Cox-Snell Residuals"),
+       ylab=ylab,main=main.title)
+  abline(0, 1, col="red", lty=2)
+  points(km$time, -log(km$surv),
+         col=c("blue","darkolivegreen4")[censored[id]+1],
+         pch=c(3,2)[censored[id]+1] )
+  legend(x = "topleft",
+         legend = c("Uncensored", "Censored"), col=c("darkolivegreen4","blue"),
+         pch=c(2,3),cex=1,xpd = TRUE,bty="L")
+
+
+  if (isTRUE(outlier.return)) {
+    if (identical(which(is.outlier), integer(0))) {
+      return(invisible(NULL))
+    } else {
+      symbols(
+        which(is.outlier),
+        Zresidual[,j][which(is.outlier)],
+        circles = rep(5, length(which(is.outlier))),
+        fg = rep('red', length(which(is.outlier))),
+        add = T,
+        inches = F
+      )
+      text(
+        which(is.outlier),
+        Zresidual[,j][which(is.outlier)],
+        pos = 1,
+        label = which(is.outlier),
+        cex = 0.8,
+        col = "red"
+      )
+    }
+
+  }
+
+  if (outlier.return) {
+    cat("Outlier Indices:", which(is.outlier), "\n")
+    invisible(list(outliers = which(is.outlier)))
+  }
+
+}
+
