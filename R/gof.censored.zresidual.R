a57cad5ccb929639e6d15bec179c75b979541271
Tingxuan Wu <txwu0310@gmail.com>
2025-08-08 15:52:54 -0600

    update

---
+++ R/gof.censored.zresidual.R
@@ -0,0 +1,16 @@
+#' A function to perform a goodness-of-fit test on censored Z-residuals.
+#' @param censored.Zresidual
+#' @export gof.censored.zresidual
+#'
+gof.censored.zresidual <- function (censored.Zresidual)
+{
+  id.negtv.inf <- which(is.infinite(censored.Zresidual) & censored.Zresidual < 0)
+  id.pos.inf <- which(is.infinite(censored.Zresidual) & censored.Zresidual > 0)
+  censored.Zresidual[id.negtv.inf]<- -1e10
+  censored.Zresidual[id.pos.inf]<- 1e10
+  censored.status<-attr(censored.Zresidual, "censored.status")
+  censored.Zresidual<- as.vector(censored.Zresidual)
+  gofTestCensored(censored.Zresidual,censored=censored.status, test = "sf",
+                  censoring.side = "right",
+                  distribution = "norm")$p.value
+}
